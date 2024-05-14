fff79b33496fc2cc2b8593f91d0a9295fae85aa4
Paul Ruiz <ptruiz@google.com>
2024-05-14 09:03:43 -0600

    Update download_models.sh

--- examples/audio_classification/ios/RunScripts/download_models.sh
+++ examples/audio_classification/ios/RunScripts/download_models.sh
@@ -19,7 +19,7 @@ TFLITE_FILE=./AudioClassification/Services/yamnet.tflite
 if test -f "$TFLITE_FILE"; then
     echo "INFO: yamnet.tflite existed. Skip downloading and use the local model."
 else
-    curl -o ${TFLITE_FILE} https://storage.googleapis.com/mediapipe-models/audio_classifier/yamnet/float32/1/yamnet.tflite
+    curl -o ${TFLITE_FILE} https://storage.googleapis.com/ai-edge/interpreter-samples/audio_classification/ios/yamnet.tflite
     echo "INFO: Downloaded yamnet.tflite to $TFLITE_FILE ."
 fi
 
@@ -28,6 +28,6 @@ TFLITE_FILE=./AudioClassification/Services/speech_commands.tflite
 if test -f "$TFLITE_FILE"; then
     echo "INFO: speech_commands.tflite existed. Skip downloading and use the local model."
 else
-    curl -o ${TFLITE_FILE} https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/audio_classification/ios/speech_commands.tflite
+    curl -o ${TFLITE_FILE} https://storage.googleapis.com/ai-edge/interpreter-samples/audio_classification/ios/speech_commands.tflite
     echo "INFO: Downloaded speech_commands.tflite to $TFLITE_FILE ."
 fi
