711e0f2dec5b139e2b44afbef4a884ff19e69d08
john-rocky <rockyshikoku@gmail.com>
2026-06-27 04:36:17 +0900

    audio_codec: Kotlin 2.2.10 (read litert 2.1.5 metadata); build + device verified

--- compiled_model_api/audio_codec/kotlin_cpu_gpu/android/build.gradle.kts
+++ compiled_model_api/audio_codec/kotlin_cpu_gpu/android/build.gradle.kts
@@ -16,5 +16,5 @@
 
 plugins {
   id("com.android.application") version "8.7.3" apply false
-  id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+  id("org.jetbrains.kotlin.android") version "2.2.10" apply false
 }
